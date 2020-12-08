8e6487c76d781846a3a9195144d25a2802247f90
Varun Mhatre <varunjmhatre@gmail.com>
2020-12-08 12:29:53 -0500

    Exception Handling
    Checked and Unchecked Exception handling using Wrapper functions and creating funtional interfaces that throws exceptions

---
+++ src/io/example4/CheckedExceptionHandling.java
@@ -0,0 +1,43 @@
+package io.example4;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class CheckedExceptionHandling {
+
+	public static void main(String[] args) {
+		
+		List<Integer> integers = Arrays.asList(3, 9, 7, 0, 10, 20);
+		integers.forEach(throwingConsumerWrapper(i -> writeToFile(i)));
+
+	}
+
+	@FunctionalInterface
+	public interface ThrowingConsumer<T, E extends Exception> 
+	{
+	    void accept(T t) throws E;
+	}
+	
+	static void writeToFile(Integer integer) throws IOException {
+	    // logic to write to file which throws IOException
+	}
+
+	static <T> Consumer<T> throwingConsumerWrapper(ThrowingConsumer<T, Exception> throwingConsumer) 
+	{
+		return i -> 
+		{
+	        try 
+	        {
+	            throwingConsumer.accept(i);
+	        } 
+	        catch (Exception ex) 
+	        {
+	            throw new RuntimeException(ex);
+	        }
+	    };
+	}
+
+	
+}
--- src/io/example4/ExceptionHandling.java
+++ src/io/example4/ExceptionHandling.java
@@ -1,10 +1,68 @@
 package io.example4;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public class ExceptionHandling {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] someNumbers = { 1, 2, 3, 4, 5 };
+		List<Integer> numberList = Arrays.asList( 1, 2, 3, 4, 5 );
+		int key = 0;
+		
+		process(someNumbers, key, BiConsumerWrapper((v,k) -> System.out.println(v/k)));
+			
+		System.out.println();
+		
+		numberList.forEach(ConsumerWrapper(i -> System.out.println(i/key), ArithmeticException.class));
 	}
 
+	private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) 
+	{	
+		for(int i : someNumbers)
+		{
+			consumer.accept(i, key);
+		}		
+	}
+
+	private static BiConsumer<Integer, Integer> BiConsumerWrapper(BiConsumer<Integer, Integer> consumer) 
+	{
+		return (v,k) -> 
+		{
+			try
+			{
+				consumer.accept(v, k);
+			}
+			catch(ArithmeticException e)
+			{
+				System.err.println("Arithmetic Exception: " + e.getMessage());
+			}
+		};
+	}
+	
+	private static <T, E extends Exception> Consumer<T> ConsumerWrapper(Consumer<T> consumer, Class<E> exception) 
+	{
+		return v -> 
+		{
+			try
+			{
+				consumer.accept(v);
+			}
+			catch(Exception e)
+			{
+				try 
+				{
+					E exCast = exception.cast(e);
+					System.err.println("Exception Occured: " + exCast.toString());
+				}
+				catch(ClassCastException ccE)
+				{
+					System.err.println("Exception Occured: " + ccE.toString());
+				}
+			}
+		};
+	}
 }
